3be28fcf19e75003cfbf0d262d7d2732cfe9ca36
vaishnavi <ggunishetty9@gmail.com>
2020-10-15 09:46:46 +0530

    Floating IP module added

---
+++ examples/floatingIP/main.tf
@@ -0,0 +1,21 @@
+#####################################################
+# Floating IP Configuration
+# Copyright 2020 IBM
+#####################################################
+
+provider "ibm" {
+}
+
+data "ibm_resource_group" "resource_group" {
+  name = (var.resource_group != null ? var.resource_group : "default")
+}
+
+module "fip" {
+  source = "../../modules/floatingIP"
+
+  name                       = var.name
+  resource_group_id          = data.ibm_resource_group.resource_group.id
+  location                   = var.location
+  target                     = (var.target != null ? var.target : null)
+  tags                       = (var.tags != null ? var.tags : [])
+}
---
+++ examples/floatingIP/variables.tf
@@ -0,0 +1,37 @@
+#####################################################
+# Floating IP Parameters
+# Copyright 2020 IBM
+#####################################################
+
+variable "name" {
+  description = "Name of the Floating IP"
+  type        = string
+}
+
+#####################################################
+# Optional Parameters
+#####################################################
+
+variable "resource_group" {
+  description = "Resource group name"
+  type        = string
+  default     = "default"
+}
+
+variable "location" {
+  description = "Floating IP Zone. This conflicts with 'target'. Give only either of one"
+  type        = string
+  default     = null
+}
+
+variable "target" {
+  description = "Target Interface ID for this Floating IP. This conflicts with 'zone'. Give only either of one"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "List of Tags for the Floating IP"
+  type        = list(string)
+  default     = null
+}
