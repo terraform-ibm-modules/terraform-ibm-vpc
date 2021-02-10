a52f060b1c8562ab623628e897a32b711f1830bb
vaishnavi <ggunishetty9@gmail.com>
2021-02-10 10:17:33 +0530

    modules shifted to use terraform v13

--- examples/volume/versions.tf
+++ examples/volume/versions.tf
@@ -4,5 +4,9 @@
 #####################################################
 
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
 }
