4964ca8e9b4ca65038e42bfd529512cb8b933820
vaishnavi <ggunishetty9@gmail.com>
2021-09-13 05:11:12 +0530

    lb pool new params added

--- examples/load-balancer/versions.tf
+++ examples/load-balancer/versions.tf
@@ -21,8 +21,7 @@ terraform {
   required_version = ">=0.13"
   required_providers {
     ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = "1.23.0"
+      source = "IBM-Cloud/ibm"
     }
   }
 }
