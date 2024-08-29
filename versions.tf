13f32d1b4b0ab7a071c168297da794a9f4bb21a3
Soaib024 <Soaib024@gmail.com>
2024-08-29 11:19:18 +0530

    add symbolic link

--- versions.tf
+++ versions.tf
@@ -1,30 +1,9 @@
-#####################################################
-# activity tracker as a service
-# Copyright 2020 IBM
-#####################################################
-
-/***************************************************
-NOTE: To source a particular version of IBM terraform
-provider version, configure the parameter `version`.
-
 terraform {
-  required_version = ">=0.13"
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-      version = "1.20.0"
-    }
-  }
-}
-If we dont configure the version parameter,
-it fetches latest provider version.
-****************************************************/
-
-terraform {
-  required_version = ">=0.13"
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-    }
-  }
+  required_version = ">= 1.3.0"
+   required_providers {
+     ibm = {
+       source  = "IBM-Cloud/ibm"
+       version = ">= 1.64.0, < 2.0.0"
+     }
+   }
 }
