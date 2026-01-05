8403816e1aef81b033df5d0115a4418725bed667
Rajat Agrawal <rajatatibm@gmail.com>
2026-01-05 11:03:28 +0000

    version range in the example

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.64.0"
+      version = ">=1.64.0"
     }
     tls = {
       source  = "hashicorp/tls"
