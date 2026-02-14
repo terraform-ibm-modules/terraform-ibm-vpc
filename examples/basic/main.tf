42d15755daea3d1a4e4e728d1bb5a2c9bae86aa5
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-14 00:05:12 +0000

    chore(deps): update dependencies

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -14,7 +14,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.7"
+  version = "1.4.8"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
